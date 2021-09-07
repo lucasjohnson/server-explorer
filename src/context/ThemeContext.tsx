cb81bfe9b8bbde58df38e5ab2357d79709bf5527
Lucas Johnson <contact@lucasjohnson.co.nz>
2021-09-07 15:09:38 +0200

    query api to fetch token with valid credentials

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const dafaultState = {
   errors: {username: false, password: false},
@@ -7,15 +7,22 @@ const dafaultState = {
 
 const ThemeContext = React.createContext(dafaultState);
 
+interface Credentials {
+  username: string;
+  password: string;
+};
+
 interface Errors {
   username: boolean;
   password: boolean;
 };
 
 const ThemeProvider: React.FC = ({ children }) => {
+  const [credentials, setCredentials] = useState<Credentials>({username: '', password: ''});
   const [errors, setErrors] = useState<Errors>({username: false, password: false});
+  const [submitted, setSubmitted] = useState<boolean>(false);
 
-  const handleFormSubmit = (event: MouseEvent, data: any): void => {
+  const handleFormSubmit = (event: MouseEvent, data: Credentials): void => {
     event.preventDefault();
 
     Object.keys(data).forEach(key => {
@@ -24,7 +31,31 @@ const ThemeProvider: React.FC = ({ children }) => {
         [key]: data[key].length === 0
       }));
     });
+
+    setCredentials(data);
+    setSubmitted(true);
   };
+
+  useEffect(() => {
+    if (submitted && credentials.username.length > 0 && credentials.password.length > 0) {
+      fetch("https://playground.tesonet.lt/v1/tokens", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(credentials)
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    };
+
+    setSubmitted(false);
+  }, [submitted]);
 
   return (
     <ThemeContext.Provider
